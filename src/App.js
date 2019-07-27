2664d1c1a58c655dd1ecda8df3a4ca72ea779e77
Kudakwashe Siziva <ksiziva@gmail.com>
2019-07-27 21:56:02 +0200

    add cypress

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class APP extends React.Component {
   render() {
-    return <div className="hello"> Hello, World! </div>;
+    return <div className="hello"> Hello, world! </div>;
   }
 }
