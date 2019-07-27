da75e28fd286588b24e424129d95383b9049c8a8
Kudakwashe Siziva <ksiziva@gmail.com>
2019-07-27 23:08:46 +0200

    Get class properties working

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default class APP extends React.Component {
+  state = { name: 'viewers' };
   render() {
-    return <div className="hello"> Hello, world! </div>;
+    return <div className="hello"> Hello, {this.state.name} </div>;
   }
 }
